aa2ce1351dc4ab9cb5f236549a8573cff2929067
elshangurbanow <elsenqurbanov50@gmail.com>
2025-05-22 18:56:33 +0000

    comimtd

--- app.js
+++ app.js
@@ -19,6 +19,33 @@ $(document).ready(function () {
             else {
                 return
             }
+
+        }
+        $("#messageyou").append(myel())
+        $("#messageme").append(myel('class="end"'))
+        $("#inputme").val("")
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+    })
+
+
+    $("#you").click(function (e) {
+        e.preventDefault()
+        function myel(myclass = "") {
+            if (!$.trim($("#inputyou").val()) == '') {
+                let vall = $("#inputyou").val()
+                    ,  newH3 = $('<h3> </h3>')
+                    ,  newDiv = $('<div class="line" > </div>')
+                    ,  bigDiv = $(`<div ${myclass} > </div>`)
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiv)
+                return bigDiv
+            }
+            else {
+                return
+            }
+
         }
         $("#messageyou").append(myel())
         $("#messageme").append(myel('class="end"'))
